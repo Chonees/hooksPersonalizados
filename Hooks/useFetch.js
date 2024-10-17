b0f7c49ce9db17519b31c059143a1272ed1b8cf5
chonees <lucasbranchini196@gmail.com>
2024-10-17 17:24:17 -0300

    renderizando los usuarios

--- Hooks/useFetch.js
+++ Hooks/useFetch.js
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useFetch = () => {
-   const [state, setState] = useState({
+export const useFetch = (url) => {
+  const [state, setState] = useState({
     data: null,
     isLoading: true,
-    errors: null,
-   });
-  
-    return {
-    data,
-    isLoading,
-    errors,
+    error: null,
+  });
+
+  const getFetch = async () => {
+    if (!url) return;
+
+    setState({
+      data: null,
+      isLoading: true,
+      error: null,
+    });
+
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error("Error en la respuesta del servidor");
+      }
+      const data = await res.json();
+      setState({
+        data,
+        isLoading: false,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        data: null,
+        isLoading: false,
+        error: error.message,
+      });
+    }
   };
+
+  useEffect(() => {
+    getFetch();
+  }, [url]);
+
+  return state; // Devolver todo el estado completo
 };
--- components/FetchApp.jsx
+++ components/FetchApp.jsx
@@ -1,28 +1,41 @@
+import { useFetch } from "../Hooks/useFetch";
 export const FetchApp = () => {
-  const { data, isLoading, errors } = useFetch();
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const { data, isLoading, error } = useFetch(url);
 
   return (
     <>
       <h2 className="h2">Lista de usuarios:</h2>
       <br />
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">id</th>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Website</td>
-          </tr>
-        </tbody>
-      </table>
+      {isLoading ? (
+        <h4>Cargando...</h4>
+      ) : error ? (
+        <h4>Hubo un error : {error}</h4>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <th scope="row">{user.id}</th>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.website}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
